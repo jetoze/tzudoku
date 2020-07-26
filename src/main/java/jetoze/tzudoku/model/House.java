c33ff00a2532928fee68f44d07b8c643724173c2
tzeth <jetoze@gmail.com>
2020-07-26 07:43:29 -0700

    Turned House into a class.

--- src/main/java/jetoze/tzudoku/model/House.java
+++ src/main/java/jetoze/tzudoku/model/House.java
@@ -1,45 +1,83 @@
 package jetoze.tzudoku.model;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
+
 import java.util.EnumSet;
 import java.util.Optional;
 import java.util.function.IntFunction;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import com.google.common.collect.ImmutableSet;
+
 /**
- * Represents the types of houses in a sudoku grid.
+ * Represents a house in a sudoku grid. 
+ * <p>
+ * A House is not tied to a specific grid, i.e. it does not contain grid-specific 
+ * information such as what values are currently entered. A House merely represents
+ * the set of positions that make up the house in any grid.
+ * 
  */
-public enum House {
-
-    ROW(Position::positionsInRow),
-    COLUMN(Position::positionsInColumn),
-    BOX(Position::positionsInBox);
-    
-    private final IntFunction<Stream<Position>> positionsSupplier;
-
-    private House(IntFunction<Stream<Position>> positionsSupplier) {
-        this.positionsSupplier = positionsSupplier;
-    }
+public class House {
     
     /**
-     * Returns a Stream of the Positions in this house.
-     * 
-     * @param num the number (1-9) of the house
+     * Represents the types of houses.
      */
-    public Stream<Position> positions(int num) {
-        return positionsSupplier.apply(num);
-    }
+    public static enum Type {
+        ROW(Position::positionsInRow),
+        COLUMN(Position::positionsInColumn),
+        BOX(Position::positionsInBox);
+        
+        private final IntFunction<Stream<Position>> positionsSupplier;
+
+        private Type(IntFunction<Stream<Position>> positionsSupplier) {
+            this.positionsSupplier = positionsSupplier;
+        }
+        
+        /**
+         * Returns a Stream of the Positions in this house.
+         * 
+         * @param num the number (1-9) of the house
+         */
+        public Stream<Position> positions(int num) {
+            return positionsSupplier.apply(num);
+        }
+    };
     
     /**
-     * Returns a set of the values not yet entered into the house of the given
-     * number.
+     * Immutable set containing all the houses in a sudoku grid.
+     */
+    public static final ImmutableSet<House> ALL = loadAllHouses();
+    
+    private static ImmutableSet<House> loadAllHouses() {
+        ImmutableSet.Builder<House> builder = ImmutableSet.builder();
+        for (House.Type type : Type.values()) {
+            IntStream.rangeClosed(1, 9)
+                .mapToObj(num -> new House(type, num))
+                .forEach(builder::add);
+        }
+        return builder.build();
+    }
+    
+    private final Type type;
+    private final int number;
+        
+    public House(Type type, int number) {
+        this.type = requireNonNull(type);
+        checkArgument(number >= 1 && number <= 9, "number must be >= 1 and <=9, but was %s", number);
+        this.number = number;
+    }
+
+    /**
+     * Returns a set of the values not yet entered into this house.
      * 
-     * @param num  the number (1-9) of the house
      * @param grid the grid
      * @return an EnumSet of the Values not yet entered
      */
-    public EnumSet<Value> getRemainingValues(int num, Grid grid) {
+    public EnumSet<Value> getRemainingValues(Grid grid) {
         EnumSet<Value> values = EnumSet.allOf(Value.class);
-        positions(num)
+        type.positions(number)
             .map(grid::cellAt)
             .map(Cell::getValue)
             .flatMap(Optional::stream)
