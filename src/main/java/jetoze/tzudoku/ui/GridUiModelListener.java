f9a1082df203622549b6f243d5f757441b1ce915
tzeth <jetoze@gmail.com>
2020-08-10 08:30:00 -0700

    Javadocs and TODO.

--- src/main/java/jetoze/tzudoku/ui/GridUiModelListener.java
+++ src/main/java/jetoze/tzudoku/ui/GridUiModelListener.java
@@ -1,9 +1,22 @@
 package jetoze.tzudoku.ui;
 
+/**
+ * Listens to changes made to a GridUiModel.
+ */
 public interface GridUiModelListener {
+    
+    // TODO: Revisit me. My only purpose at the moment is to trigger repaints of
+    // the grid. Is there a better way?
 
+    /**
+     * Notifies this listener that a new puzzle was loaded into the model.
+     */
     default void onNewPuzzleLoaded() {/**/}
     
+    /**
+     * Notifies this listener that the state of one or more cells changed. This could mean
+     * changes to the cell's value, pencil marks, or color.
+     */
     default void onCellStateChanged() {/**/}
 
 }
