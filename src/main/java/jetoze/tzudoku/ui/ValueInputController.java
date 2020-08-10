fa62d3fe483292db8177b1b748b8141adbf0ff62
tzeth <jetoze@gmail.com>
2020-08-10 09:31:38 -0700

    When building a puzzle, automatically select the next cell when a value has been entered into the grid.

--- src/main/java/jetoze/tzudoku/ui/ValueInputController.java
+++ src/main/java/jetoze/tzudoku/ui/ValueInputController.java
@@ -1,6 +1,6 @@
 package jetoze.tzudoku.ui;
 
-import static com.google.common.base.Preconditions.*;
+import static com.google.common.base.Preconditions.checkState;
 import static java.util.Objects.requireNonNull;
 
 import java.awt.event.KeyEvent;
@@ -10,6 +10,7 @@ import jetoze.attribut.Property;
 import jetoze.gunga.KeyBindings;
 import jetoze.gunga.KeyStrokes;
 import jetoze.tzudoku.model.CellColor;
+import jetoze.tzudoku.model.Position;
 import jetoze.tzudoku.model.Value;
 
 /**
@@ -22,28 +23,24 @@ public class ValueInputController { // TODO: Come up with a better name. "Value"
      * Creates a ValueInputController for use when solving a puzzle.
      */
     public static ValueInputController forSolving(GridUiModel model) {
-        return new ValueInputController(model, true);
+        return new ValueInputController(model, Purpose.SOLVING);
     }
 
     /**
      * Creates a ValueInputController for use when building a puzzle.
      */
     public static ValueInputController forBuilding(GridUiModel model) {
-        return new ValueInputController(model, false);
+        return new ValueInputController(model, Purpose.BUILDING);
     }
     
     private final GridUiModel model;
-    private final boolean supportsPencilMarks;
+    private final Purpose purpose;
     private final Property<EnterValueMode> enterValueMode = Properties.newProperty(
             "enterValueMode", EnterValueMode.NORMAL);
-    
-    // TODO: Support listener that is notified when a new value is entered into the model?
-    //       That would allow us to implement the puzzle builder mode where the next cell is
-    //       automatically selected when a new value is entered.
 
-    private ValueInputController(GridUiModel model, boolean supportsPencilMarks) {
+    private ValueInputController(GridUiModel model, Purpose purpose) {
         this.model = requireNonNull(model);
-        this.supportsPencilMarks = supportsPencilMarks;
+        this.purpose = requireNonNull(purpose);
     }
 
     public Property<EnterValueMode> getEnterValueModeProperty() {
@@ -58,14 +55,14 @@ public class ValueInputController { // TODO: Come up with a better name. "Value"
         requireNonNull(value);
         switch (enterValueMode.get()) {
         case NORMAL:
-            model.enterValue(value);
+            enterValue(value);
             break;
         case CORNER_PENCIL_MARK:
-            checkState(supportsPencilMarks, "Pencil marks are not supported");
+            checkState(this.purpose == Purpose.SOLVING, "Pencil marks are not supported when building puzzles");
             model.toggleCornerMark(value);
             break;
         case CENTER_PENCIL_MARK:
-            checkState(supportsPencilMarks, "Pencil marks are not supported");
+            checkState(this.purpose == Purpose.SOLVING, "Pencil marks are not supported when building puzzles");
             model.toggleCenterMark(value);
             break;
         case COLOR:
@@ -76,13 +73,24 @@ public class ValueInputController { // TODO: Come up with a better name. "Value"
             throw new RuntimeException("Unknown mode: " + enterValueMode.get());
         }
     }
+    
+    private void enterValue(Value value) {
+        model.enterValue(value);
+        if (purpose == Purpose.BUILDING) {
+            model.getLastSelectedCell().ifPresent(c -> {
+                Position currentPosition = c.getPosition();
+                Position nextPosition = model.getNavigationMode().right(currentPosition);
+                model.selectCellAt(nextPosition);
+            });
+        }
+    }
 
     public void registerActions(KeyBindings keyBindings) {    
         for (Value v : Value.values()) {
             keyBindings.add(KeyStrokes.forKey(KeyEvent.VK_0 + v.toInt()), "enter-" + v, 
                     () -> updateModel(v));
         }
-        if (supportsPencilMarks) {
+        if (this.purpose == Purpose.SOLVING) {
             keyBindings.add(KeyStrokes.forKey(KeyEvent.VK_N), "normal-value-mode",
                     () -> setEnterValueMode(EnterValueMode.NORMAL));
             keyBindings.add(KeyStrokes.forKey(KeyEvent.VK_R), "corner-value-mode",
@@ -90,6 +98,11 @@ public class ValueInputController { // TODO: Come up with a better name. "Value"
             keyBindings.add(KeyStrokes.forKey(KeyEvent.VK_C), "center-value-mode",
                     () -> setEnterValueMode(EnterValueMode.CENTER_PENCIL_MARK));
         }
+    }
+    
+    
+    private static enum Purpose {
+        SOLVING, BUILDING
     }
 
 }
