f1f517daee9f441c796ae4f111d5a07ca6110b86
tzeth <jetoze@gmail.com>
2020-08-10 19:08:24 -0700

    Finished first implementation of XyzWing.findNext(). Test is passing.

--- src/main/java/jetoze/tzudoku/hint/XyzWing.java
+++ src/main/java/jetoze/tzudoku/hint/XyzWing.java
@@ -1,16 +1,23 @@
 package jetoze.tzudoku.hint;
 
-import static java.util.Objects.*;
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static java.util.Objects.requireNonNull;
 
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.Optional;
-
-import static com.google.common.base.Preconditions.*;
-
 import java.util.Set;
+import java.util.stream.Stream;
 
+import javax.annotation.Nullable;
+
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 
+import jetoze.tzudoku.model.Cell;
 import jetoze.tzudoku.model.Grid;
 import jetoze.tzudoku.model.Position;
 import jetoze.tzudoku.model.Value;
@@ -51,7 +58,129 @@ public class XyzWing extends EliminatingHint {
 
     public static Optional<XyzWing> findNext(Grid grid) {
         requireNonNull(grid);
-        return Optional.empty();
+        return new Detector(grid).find();
     }
 
+    
+    private static class Detector {
+        private final Grid grid;
+        
+        public Detector(Grid grid) {
+            this.grid = grid;
+        }
+        
+        public Optional<XyzWing> find() {
+            return collectPossibleCenterCells()
+                    .map(this::check)
+                    .filter(Objects::nonNull)
+                    .findFirst();
+        }
+        
+        private Stream<Center> collectPossibleCenterCells() {
+            return Position.all()
+                    .map(p -> Center.examine(grid, p))
+                    .filter(Objects::nonNull);
+        }
+        
+        @Nullable
+        private XyzWing check(Center centerCell) {
+            ImmutableList<Wing> possibleWings = centerCell.collectPossibleWings(grid);
+            if (possibleWings.size() < 2) {
+                // We need two wing cells
+                return null;
+            }
+            // XXX: This n^2 algorithm isn't satisfying, but the number of wing cells is bounded
+            // so we are at least not going to blow up.
+            for (int i = 0; i < possibleWings.size() - 1; ++i) {
+                for (int j = i + 1; j < possibleWings.size(); ++j) {
+                    Wing wing1 = possibleWings.get(i);
+                    Wing wing2 = possibleWings.get(j);
+                    Value sharedValue = wing1.getSingleSharedValue(wing2);
+                    if (sharedValue != null) {
+                        // We have found a center and two wings that fulfills the XYZ-wing requirements.
+                        // Last thing to check is if there are any cells that sees all these three cells,
+                        // and have the shared value we just found as a candidate.
+                        ImmutableSet<Position> targets = matchingPositionsSeenByAllThreeCells(centerCell, wing1, wing2, sharedValue);
+                        if (!targets.isEmpty()) {
+                            return new XyzWing(grid, centerCell.position, ImmutableSet.of(wing1.position, wing2.position), 
+                                    sharedValue, targets);
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+        
+        private ImmutableSet<Position> matchingPositionsSeenByAllThreeCells(Center center, Wing wing1, Wing wing2, Value value) {
+            return Position.all()
+                    .filter(p -> !p.equals(center.position) && !p.equals(wing1.position) && !p.equals(wing2.position))
+                    .filter(p -> p.sees(center.position) && p.sees(wing1.position) && p.sees(wing2.position))
+                    .filter(HintUtils.isCandidate(grid, value))
+                    .collect(toImmutableSet());
+        }
+    }
+    
+    
+    private static class Center {
+        private final Position position;
+        private final ImmutableSet<Value> candidates;
+        
+        public Center(Position position, ImmutableSet<Value> candidates) {
+            assert candidates.size() == 3;
+            this.position = position;
+            this.candidates = candidates;
+        }
+        
+        @Nullable
+        public static Center examine(Grid grid, Position position) {
+            Cell cell = grid.cellAt(position);
+            if (!cell.hasValue()) {
+                ImmutableSet<Value> candidates = cell.getCenterMarks().getValues();
+                if (candidates.size() == 3) {
+                    return new Center(position, candidates);
+                }
+            }
+            return null;
+        }
+        
+        public ImmutableList<Wing> collectPossibleWings(Grid grid) {
+            return position.seenBy()
+                    .map(p -> Wing.examine(grid, p, this))
+                    .filter(Objects::nonNull)
+                    .collect(toImmutableList());
+        }
+    }
+    
+    private static class Wing {
+        private final Position position;
+        private final ImmutableSet<Value> candidates;
+        
+        public Wing(Position position, ImmutableSet<Value> candidates) {
+            assert candidates.size() == 2;
+            this.position = position;
+            this.candidates = candidates;
+        }        
+        
+        @Nullable
+        public static Wing examine(Grid grid, Position position, Center center) {
+            Cell cell = grid.cellAt(position);
+            if (!cell.hasValue()) {
+                ImmutableSet<Value> candidates = cell.getCenterMarks().getValues();
+                if (candidates.size() == 2 && center.candidates.containsAll(candidates)) {
+                    return new Wing(position, candidates);
+                }
+            }
+            return null;
+        }
+        
+        @Nullable
+        public Value getSingleSharedValue(Wing other) {
+            Set<Value> intersection = Sets.intersection(this.candidates, other.candidates);
+            return intersection.size() == 1
+                    ? intersection.iterator().next()
+                    : null;
+        }
+    }
+    
+    
 }
